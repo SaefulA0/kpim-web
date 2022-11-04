162c3287be64cbd103d630326e7a97fbccf6d3f7
Saeful_A <saefulanwaroktariansah@gmail.com>
2022-11-04 20:33:08 +0700

    update 04/11/2022

---
+++ components/table/tableAnggota/Columns.js
@@ -0,0 +1,34 @@
+export const COLUMNS = [
+  {
+    Header: "id",
+    accessor: "id",
+  },
+  {
+    Header: "Username",
+    accessor: "username",
+  },
+  {
+    Header: "Nama Anggota",
+    accessor: "nama_anggota",
+  },
+  {
+    Header: "Jabatan",
+    accessor: "jabatan",
+  },
+  {
+    Header: "Keanggotaan",
+    accessor: "keanggotaan",
+  },
+  {
+    Header: "Status",
+    accessor: "status",
+  },
+  // {
+  //   Header: "Country",
+  //   accessor: "country",
+  // },
+  // {
+  //   Header: "Phone",
+  //   accessor: "phone",
+  // },
+];
